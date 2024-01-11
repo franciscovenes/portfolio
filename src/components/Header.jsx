503255a3821ac31f4346823939abcd7cab3dc5b9
Pancho Venes <francisco.venes@gmail.com>
2024-01-11 20:19:20 +0000

    Menu dynamics simplified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,14 @@
 import { FiMenu } from "react-icons/fi";
 import { AiOutlineClose } from "react-icons/ai";
-import { useState, useEffect } from "react";
-import Fade from "./Fade";
-import { useLayoutEffect } from "react";
+import { useState } from "react";
 
 export default function Header() {
   const [mobileMenu, setMobileMenu] = useState(false);
-  const [mobileUI, setMobileUI] = useState(null);
 
   const handleMobileBtn = () => {
     setMobileMenu((prevState) => !prevState);
+    console.log("Clicked!");
   };
-
-  useLayoutEffect(() => {
-    if (window.innerWidth >= 768) {
-      setMobileUI(false);
-    } else {
-      setMobileUI(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 768) {
-        setMobileUI(false);
-        setMobileMenu(false);
-      } else {
-        setMobileUI(true);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  if (mobileUI === null) return null;
 
   return (
     <header className="header--container">
@@ -46,32 +17,45 @@ export default function Header() {
         <h1>Francisco Venes</h1>
       </a>
 
-      <Fade show={mobileMenu && mobileUI}>
-        <AiOutlineClose
-          onClick={handleMobileBtn}
-          className="header--nav--mobile"
-        />
-      </Fade>
-      <Fade show={!mobileMenu && mobileUI}>
-        <FiMenu onClick={handleMobileBtn} className="header--nav--mobile" />
-      </Fade>
+      <AiOutlineClose
+        onClick={handleMobileBtn}
+        className={`header--nav--exit-btn ${
+          mobileMenu ? "" : "header--nav--hide-btn"
+        }`}
+      />
 
-      <Fade show={(mobileMenu && mobileUI) || !mobileUI}>
-        <ul onClick={mobileUI ? handleMobileBtn : () => false}>
-          <li>
-            <a href="#hero">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </Fade>
+      <FiMenu
+        onClick={handleMobileBtn}
+        className={`header--nav--open-btn ${
+          !mobileMenu ? "" : "header--nav--hide-btn"
+        }`}
+      />
+
+      <ul className={mobileMenu ? "header--nav--mobile" : ""}>
+        <li>
+          <a onClick={mobileMenu ? handleMobileBtn : undefined} href="#hero">
+            Home
+          </a>
+        </li>
+        <li>
+          <a onClick={mobileMenu ? handleMobileBtn : undefined} href="#about">
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={mobileMenu ? handleMobileBtn : undefined}
+            href="#projects"
+          >
+            Projects
+          </a>
+        </li>
+        <li>
+          <a onClick={mobileMenu ? handleMobileBtn : undefined} href="#contact">
+            Contact
+          </a>
+        </li>
+      </ul>
     </header>
   );
 }
